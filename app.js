bb271d41ca711f2b5b9be1f6ac678bd6721928b9
hyonjnk@gmail.com <Pururin120>
2023-04-05 03:51:07 +0900

    Login form 完了

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const loginButton = document.querySelector('#login-form button');
+const link=document.querySelector('a');
 
 function onLoginBtnClick(event) {
     event.preventDefault();
     console.log(event);
 }
 
-loginForm.addEventListener('submit', onLoginBtnClick);
+function handleLinkClik(event){
+    event.preventDefault();
+    console.log(event);
+}
 
-onLoginBtnClick()
+loginForm.addEventListener('submit', onLoginBtnClick);
+link.addEventListener('click',handleLinkClik);
