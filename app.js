89d4c2e09853152be09a4d93105f5386d469c460
hyonjnk@gmail.com <Pururin120>
2023-04-05 03:52:21 +0900

    hidden classを持っている要素を非表示にする

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const loginButton = document.querySelector('#login-form button');
-const link=document.querySelector('a');
+const greeting = document.querySelector('#greeting');
 
-function onLoginBtnClick(event) {
-    event.preventDefault();
-    console.log(event);
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = "username"; ///変数名を揃えるため、あとで変更しやすいように変数に代入しておく
+
+function onLoginSubmit(event) {
+    event.preventDefault(); ///ブラウザのデフォルトの動作を止める
+    loginForm.classList.add(HIDDEN_CLASSNAME); ///loginFormを隠す
+    const username = loginInput.value; ///loginInputのvalueをを変数usernameに代入
+    localStorage.setItem(USERNAME_KEY, username); ///usernameというKeynameで変数usernameの値を保存
+    paintGreetings(username);
 }
 
-function handleLinkClik(event){
-    event.preventDefault();
-    console.log(event);
+function paintGreetings(username) {
+    greeting.innerText = `こんにちは！${username}さん`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', onLoginBtnClick);
-link.addEventListener('click',handleLinkClik);
+const savedUsername = localStorage.getItem(USERNAME_KEY);///usernameというKeynameで保存されている値を取得
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);///submitボタンが押されたらonLoginSubmitを実行
+} else {
+   paintGreetings(savedUsername)
+}
